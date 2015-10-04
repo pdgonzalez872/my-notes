306563999bc1e8058786fb02752ec9dcb4888922
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-04 15:21:05 -0500

    Finish add functionality

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,6 @@
 (function(){
 
-  var notes = [
-    {
-      id: '1',
-      title: 'First Note',
-      description: 'This is my first note.'
-    },
-    {
-      id: '2',
-      title: 'Second Note',
-      description: 'This is my second note.'
-    }
-  ];
+  var notes = [];
 
   var app = angular.module('starter', ['ionic']);
 
