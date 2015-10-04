1488c859238f3552558596947a9adbaf15b4527d
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-04 13:44:40 -0500

    Create views, use navigation to move across routes

--- www/js/app.js
+++ www/js/app.js
@@ -1,35 +1,47 @@
-// Ionic Starter App
+var app = angular.module('starter', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic'])
-
-app.controller('ListController', function($scope){
+app.controller('ListCtrl', function($scope) {
 
   $scope.notes = [
     {
-      title: "note 1",
-      description: "This is my 1 note"
+      title: 'First Note',
+      description: 'This is my first note.'
     },
     {
-      title: "note2",
-      description: "This is my 2 note"
+      title: 'Second Note',
+      description: 'This is my second note.'
     }
   ];
 
+});
 
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  // adds a state
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'templates/list.html'
+  });
+
+  // adds another state
+  $stateProvider.state('edit', {
+    url: "/edit",
+    templateUrl: "templates/edit.html"
+  });
+
+
+  // if the url doesn't match anything defined before, then redirect to list.
+  // REDIRECT!
+  $urlRouterProvider.otherwise('/list');
 });
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
